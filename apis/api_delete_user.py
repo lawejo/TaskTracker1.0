b58e2b7a23f26da010b5169a0f4b55521dde8d89
lawejo <lwj1993@gmail.com>
2023-12-11 17:33:04 +0100

    checking out different branch

--- apis/api_delete_user.py
+++ apis/api_delete_user.py
@@ -8,14 +8,15 @@ def _():
         error_target = 'Failed to locate this user in database'
         db = x.db()
         user = x.get_cookie_user()
-        print('*'*40)
-        print(f'FROM api_delete_user.py = {user}')
-        print('*'*40)
         # Check for if they are logged in (cookie user) and if they are admin (user role = 1)
         if not user or not user['user_role'] == '1':
-            return template("access-denied", message='Access denied')
-        target_user_id = request.forms.get('target_id').strip()
+            return template("access-denied", message='Access denied')  
+    # Linje 15 er i tilfældet at en hacker får adgang til at sende formlen til serveren, men sender tomme felter med        
+        if not request.forms.get('admin_password') or not request.forms.get('target_id'):
+            raise Exception(400,'Form submission error. Please make sure all required fields are filled.')
+        target_user_id =  request.forms.get('target_id').strip()
         admin_password = request.forms.get('admin_password').strip()
+   
         verify_target = db.execute('SELECT COUNT(*) FROM users WHERE user_id = ?',(target_user_id,)).fetchone()
 
         if verify_target["COUNT(*)"] == 0:
