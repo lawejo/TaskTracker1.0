4be36d9c0b68ef5e97c28bb8c19ba9cae9139b32
lawejo <lwj1993@gmail.com>
2023-12-13 08:36:58 +0100

    user bliver slettet, modal lukker, få mangler

--- !test.sql
+++ !test.sql
@@ -1,7 +1,7 @@
 SELECT * FROM users;
 DELETE FROM users WHERE user_firstname = 'Angela';
 
-UPDATE users SET user_verified_at = 1 WHERE user_firstname = 'Hans';
-UPDATE users SET user_role = 1 WHERE user_firstname = 'peder';
+UPDATE users SET user_verified_at = 1 WHERE user_firstname = 'Lasse';
+UPDATE users SET user_role = 1 WHERE user_firstname = 'Lasse';
 
 SELECT * FROM users WHERE NOT user_role = 0;
--- js/delete-user.js
+++ js/delete-user.js
@@ -1,23 +1,27 @@
 "use strict"
-const deleteUserModal = document.querySelector('.modal-delete-user')
+let deleteUserModal;
+
 async function deleteUser() {
     const frm = event.target // the form
     const conn = await fetch("/api-delete-user", { method: "DELETE", body: new FormData(frm) })
     const data = await conn.json() // to get plain text
-    console.log(data);
+    data.info == 'ok' ? success() : error();
+
+    function success() {
+        closeDeleteUserModal()
+        // #TODO: Tilføj error handling4
+        // #TODO: Fjern html element
+
+    }
+    function error() {
+        console.log(`error handling`);
+    }
 }
 
 function openDeleteUserModal() {
+    deleteUserModal = event.target.previousElementSibling.previousElementSibling
     deleteUserModal.classList.remove('hidden')
-
     const userId = event.target.value
-
-    const userFirstName = document.querySelector(`.user_firstname_${userId}`).textContent
-    console.log(userFirstName);
-    const userLastName = document.querySelector(`.user_lastname_${userId}`).textContent
-    console.log(userLastName);
-    console.log(document.querySelector(`.username_${userId}`));
-    document.querySelector(`.username_${userId}`).innerHTML = `${userFirstName}  ${userLastName}`
 }
 function closeDeleteUserModal() {
     deleteUserModal.classList.add("hidden")
